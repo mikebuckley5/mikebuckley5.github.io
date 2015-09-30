f5ddd969278cf2d8bd0d04004743b84620f9a64d
mikebuckley5 <que.onda.buckley@gmail.com>
2015-09-29 23:00:26 -0600

    initial commit

---
+++ js/route.js
@@ -0,0 +1,21 @@
+var app = angular.module('app', ['ui.router']);
+app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.otherwise('/landing');
+    $stateProvider
+        .state('landing', {
+            url: '/landing',
+            templateUrl: '../templates/landing.html'
+        })
+        .state('about', {
+            url: '/about',
+            templateUrl: '../templates/about.html'
+        })
+        .state('projects', {
+            url: '/projects',
+            templateUrl: '../templates/projects.html'
+        })
+        .state('contact', {
+            url: '/contact',
+            templateUrl: '../templates/contact.html'
+        });
+});
